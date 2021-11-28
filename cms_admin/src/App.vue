510883ada91d3cdd4673b7518d55cac27475f84a
jiangnan <896333574@qq.com>
2021-11-28 16:10:53 +0800

    11

--- cms_admin/src/App.vue
+++ cms_admin/src/App.vue
@@ -1,11 +1,12 @@
 <template>
-    <!--    路由出口函数    -->
-    <router-view/>
+  <router-link to="/login">login</router-link>
+  <!--    路由出口函数    -->
+  <router-view/>
 </template>
 
 <script>
 export default {
-    name: 'App'
+  name: 'App'
 }
 
 </script>
--- cms_admin/src/views/login/index.vue
+++ cms_admin/src/views/login/index.vue
@@ -1,64 +1,64 @@
 <template>
-    <div class="login-container">
-        <div class="logo-img"></div>
-        <el-form class="login-from" ref="form" :model="form">
-            <el-form-item>
-                <el-input v-model="form.username" placeholder="Please input username"></el-input>
-            </el-form-item>
-            <el-form-item>
-                <el-input v-model="form.password" placeholder="Please input password" show-password/>
-            </el-form-item>
-            <el-form-item>
-                <el-checkbox v-model="checked" label="我已阅读并同意用户协议隐私条款"></el-checkbox>
-            </el-form-item>
-            <el-form-item>
-                <el-button class="login-btn" type="primary" @click="onSubmit">登录</el-button>
-            </el-form-item>
-        </el-form>
-    </div>
+  <div class="login-container">
+    <el-image class="logo" src="./login-logo.png" fit="none"></el-image>
+    <el-form class="login-from" ref="form" :model="form">
+      <el-form-item>
+        <el-input v-model="form.username" placeholder="Please input username"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-input v-model="form.password" placeholder="Please input password" show-password/>
+      </el-form-item>
+      <el-form-item>
+        <el-checkbox v-model="checked" label="我已阅读并同意用户协议隐私条款"></el-checkbox>
+      </el-form-item>
+      <el-form-item>
+        <el-button class="login-btn" type="primary" @click="onSubmit">登录</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "LoginIndex",
-    data() {
-        return {
-            form: {
-                username: '',
-                password: '',
-            },
-            checked: false,
-            src: '',
-        }
+  name: "LoginIndex",
+  data() {
+    return {
+      form: {
+        username: '',
+        password: '',
+      },
+      checked: false,
+      src: '',
+    }
+  },
+  methods: {
+    onSubmit() {
+      console.log('submit!')
     },
-    methods: {
-        onSubmit() {
-            console.log('submit!')
-        },
-    },
+  },
 }
 </script>
 
-<style scoped lang="less">
+<style lang="less">
 .login-container {
-    max-width: 500px;
-    align-items: center;
-    background-color: #acc6da;
+  max-width: 400px;
+  margin: 5% auto;
+  box-shadow: 0 0 24px rgba(0, 0, 0, 0.15);
+  background-color: rgb(245, 246, 252);
+  border-radius: 24px;
+  padding: 24px 14px;
 
-    .logo-img{
-        background-image: url("login-logo.png");
-        height: 119px;
-        width: 320px;
-        margin: 20px auto auto;
-    }
+  .logo{
+    padding-left: 50px;
+  }
 
-    .login-from {
-        padding: 50px;
-    }
+  .login-from {
+    padding: 25px;
+  }
 
-    .login-btn {
-        width: 100%;
-    }
+  .login-btn {
+    width: 100%;
+  }
 }
 
 </style>
