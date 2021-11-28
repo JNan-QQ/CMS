0bdab36a79ee6f86bea7cace3b091a1d10c60cfc
jiangnan <896333574@qq.com>
2021-11-28 22:45:16 +0800

    11

--- cms_admin/src/utils/request.js
+++ cms_admin/src/utils/request.js
@@ -6,7 +6,7 @@ import axios from 'axios'
 // 创建一个 axios 实例
 const request = axios.create({
     // 基础地址 host
-    baseURL: 'http://127.0.0.1:8210'
+    // baseURL: 'http://127.0.0.1:8210'
 })
 
 export default request
--- cms_admin/src/views/login/index.vue
+++ cms_admin/src/views/login/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="login-container">
-    <el-image class="logo" src="./login-logo.png" fit="none"></el-image>
-    <el-form class="login-from" ref="form" :model="form">
+    <el-image class="logo" fit="none" src="./login-logo.png"></el-image>
+    <el-form ref="form" :model="form" class="login-from">
       <el-form-item>
         <el-input v-model="form.username" placeholder="Please input username"></el-input>
       </el-form-item>
@@ -12,28 +12,58 @@
         <el-checkbox v-model="checked" label="我已阅读并同意用户协议隐私条款"></el-checkbox>
       </el-form-item>
       <el-form-item>
-        <el-button class="login-btn" type="primary" @click="onSubmit">登录</el-button>
+        <el-button :loading="loading" class="login-btn" type="primary" @click="onSubmit">登录</el-button>
       </el-form-item>
     </el-form>
   </div>
 </template>
 
 <script>
+import {ElMessage} from "element-plus";
+import request from "../../utils/request";
+
 export default {
   name: "LoginIndex",
   data() {
     return {
       form: {
+        action: 'signin',
         username: '',
         password: '',
       },
       checked: false,
-      src: '',
+      loading: false,
     }
   },
   methods: {
     onSubmit() {
-      console.log('submit!')
+      // 禁用登陆按钮
+      this.loading = true
+
+      // 判断是否同意条款
+      if (this.checked===false){
+        ElMessage('请阅读同意条款后登陆。')
+        return this.loading = false
+      }
+
+      // 发起登陆请求
+      request.post('/api/sign/', this.form).then(
+          function (response) {
+            const data = response.data
+            if (data['ret'] === 0) {
+              ElMessage({
+                message: '登陆成功！欢迎您：' + data['realName'],
+                type: 'success',
+              })
+            } else {
+              ElMessage({
+                message: '登陆失败！msg：' + data['msg'],
+                type: 'warning',
+              })
+            }
+          }
+      ).catch(reason => ElMessage.error('服务器错误'))
+      this.loading = false
     },
   },
 }
@@ -46,9 +76,9 @@ export default {
   box-shadow: 0 0 24px rgba(0, 0, 0, 0.15);
   background-color: rgb(245, 246, 252);
   border-radius: 24px;
-  padding: 24px 14px;
+  padding: 24px 24px;
 
-  .logo{
+  .logo {
     padding-left: 50px;
   }
 
